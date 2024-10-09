76e3dcc5dbb0ad77245bc0751b09a3da51680daa
Minh Pham <pcminh0505@gmail.com>
2024-10-09 12:33:25 +0700

    chore: add DEVLOG and default urls

---
+++ docs/DEVLOG.md
@@ -0,0 +1,25 @@
+# Buildathon DEVLOG
+
+---
+## 2024-10-07
+Idea initiation - multichain-scoring for personalized recommendations of product for user to have more motivation onboarding to Base
+- Extension of [onchainscore.xyz](https://www.onchainscore.xyz/) and [Basename on-chain score](https://x.com/base/status/1841911856656708073), multi-aggregation, product-oriented (DeFi, GameFi, NFT, SocialFi,...) metrics
+
+Whiteboard: [Excalidraw](https://excalidraw.com/#room=4d025e9394a150d042b9,SLby7Ri-UjHaGsRpWElsjQ)
+![draft-idea](./assets/draft-idea.png)
+
+
+Technical preparation:
+- evm-scan data fetching ([Etherscan](https://etherscan.io/) and its sub-products on multichains): From ETH, Base, OP, BSC, ARB -> Setup API key and router
+- Token & NFT Activity: [Alchemy](https://www.alchemy.com/) and [Reservoir](https://reservoir.tools/)
+- Vietnamese-oriented chain: [Viction](https://www.viction.xyz/) (explorer scan) and [Dagora](https://dagora.xyz/) (NFT marketplace)
+
+Done:
+- Init repo with [OnChainKit](onchainkit.xyz)
+
+## 2024-10-08
+Done: 
+- API router for data fetching (simple aggregation)
+- Convert [ENS](https://dagora.xyz/) (.eth) and [OneID](https://www.oneid.xyz/) to wallet address
+
+Define scoring metrics and UI/UX (expected insights for user)
--- src/constants/explorers.ts
+++ src/constants/explorers.ts
@@ -1,27 +1,33 @@
-export const ETHERSCAN_API_BASE_URL = process.env
-  .NEXT_PUBLIC_ETHERSCAN_API_BASE_URL as string;
+export const ETHERSCAN_API_BASE_URL =
+  (process.env.NEXT_PUBLIC_ETHERSCAN_API_BASE_URL as string) ||
+  'https://api.etherscan.io/api';
 export const ETHERSCAN_API_KEY = process.env
   .NEXT_PUBLIC_ETHERSCAN_API_KEY as string;
 
-export const BASESCAN_API_BASE_URL = process.env
-  .NEXT_PUBLIC_BASESCAN_API_BASE_URL as string;
+export const BASESCAN_API_BASE_URL =
+  (process.env.NEXT_PUBLIC_BASESCAN_API_BASE_URL as string) ||
+  'https://api.basescan.org/api';
 export const BASESCAN_API_KEY = process.env
   .NEXT_PUBLIC_BASESCAN_API_KEY as string;
 
-export const OPTIMISMSCAN_API_BASE_URL = process.env
-  .NEXT_PUBLIC_OPTIMISMSCAN_API_BASE_URL as string;
+export const OPTIMISMSCAN_API_BASE_URL =
+  (process.env.NEXT_PUBLIC_OPTIMISMSCAN_API_BASE_URL as string) ||
+  'https://api-optimistic.etherscan.io/api';
 export const OPTIMISMSCAN_API_KEY = process.env
   .NEXT_PUBLIC_OPTIMISMSCAN_API_KEY as string;
 
-export const ARBSCAN_API_BASE_URL = process.env
-  .NEXT_PUBLIC_ARBSCAN_API_BASE_URL as string;
+export const ARBSCAN_API_BASE_URL =
+  (process.env.NEXT_PUBLIC_ARBSCAN_API_BASE_URL as string) ||
+  'https://api.bscscan.com/api';
 export const ARBSCAN_API_KEY = process.env
   .NEXT_PUBLIC_ARBSCAN_API_KEY as string;
 
-export const BSCSCAN_API_BASE_URL = process.env
-  .NEXT_PUBLIC_BSCSCAN_API_BASE_URL as string;
+export const BSCSCAN_API_BASE_URL =
+  (process.env.NEXT_PUBLIC_BSCSCAN_API_BASE_URL as string) ||
+  'https://api.arbiscan.io/api';
 export const BSCSCAN_API_KEY = process.env
   .NEXT_PUBLIC_BSCSCAN_API_KEY as string;
 
-export const VICSCAN_API_BASE_URL = process.env
-  .NEXT_PUBLIC_VICSCAN_API_BASE_URL as string;
+export const VICSCAN_API_BASE_URL =
+  (process.env.NEXT_PUBLIC_VICSCAN_API_BASE_URL as string) ||
+  'https://www.vicscan.xyz/api';
