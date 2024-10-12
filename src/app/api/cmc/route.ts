1c307e62ff236808479556c55cc39b026a5d1d76
Minh Pham <pcminh0505@gmail.com>
2024-10-12 13:34:22 +0700

    fix: Update CMC token list

--- src/app/api/cmc/route.ts
+++ src/app/api/cmc/route.ts
@@ -1,10 +1,18 @@
 import { CMC_API_BASE_URL, CMC_API_KEY } from '@/constants/web3Infra';
+import cmcIDlist from '@data/cmcIDList.json';
 import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const tokens = searchParams.get('tokens');
-  const query = `symbol=${tokens}&aux=tags,date_added`;
+
+  const tokenSymbolList = tokens?.split(',') || [];
+  const tokenIdList = tokenSymbolList.map(
+    (symbol) =>
+      cmcIDlist.data.find((token) => token.symbol === symbol)?.id || '',
+  );
+
+  const query = `id=${tokenIdList.filter(Boolean).join(',')}&aux=tags,date_added`;
 
   const res = await fetch(
     `${CMC_API_BASE_URL}/v1/cryptocurrency/quotes/latest?${query}`,
