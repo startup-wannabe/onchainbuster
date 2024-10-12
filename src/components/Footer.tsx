2192fb4c5daf68bd3b7c04f6fd2742a249af09ab
Minh Pham <pcminh0505@gmail.com>
2024-10-13 00:13:12 +0700

    chore: Update footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,16 @@
 'use client';
 
 import ArrowSvg from '@assets/svg/ArrowSvg';
-import {
-  DISCORD_LINK,
-  FIGMA_LINK,
-  GITHUB_LINK,
-  ONCHAINKIT_LINK,
-  TWITTER_LINK,
-} from '@constants/links';
+import { GITHUB_LINK, ONCHAINKIT_LINK } from '@constants/links';
 
-const docLinks = [
-  { href: ONCHAINKIT_LINK, title: 'Docs' },
-  { href: GITHUB_LINK, title: 'Github' },
-  { href: DISCORD_LINK, title: 'Discord' },
-  { href: FIGMA_LINK, title: 'Figma' },
-  { href: TWITTER_LINK, title: 'X' },
-];
+const docLinks = [{ href: GITHUB_LINK, title: 'Github' }];
 
 export default function Footer() {
   return (
-    <footer className="mt-auto mb-2 flex w-full flex-col flex-col-reverse justify-between gap-2 md:mt-8 md:mb-6 md:flex-row">
+    <footer className="mt-auto mb-2 flex w-full flex-col-reverse justify-between gap-2 md:mt-8 md:mb-6 md:flex-row">
       <aside className="flex items-center pt-2 md:pt-0">
         <h3 className="mr-2 mb-2 text-m md:mb-0">
-          Built with love by{' '}
+          Built with{' '}
           <a
             href={ONCHAINKIT_LINK}
             target="_blank"
