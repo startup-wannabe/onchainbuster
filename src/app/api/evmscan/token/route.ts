071186ac892dd8958db86feda69d09cb147dbe65
Minh Pham <pcminh0505@gmail.com>
2024-10-19 23:46:13 +0700

    fix: Duplicate activity

--- src/app/api/evmscan/token/route.ts
+++ src/app/api/evmscan/token/route.ts
@@ -1,6 +1,7 @@
 import {
   ARBSCAN_API_BASE_URL,
   ARBSCAN_API_KEY,
+  BASESCAN_API_BASE_SEPOLIA_URL,
   BASESCAN_API_BASE_URL,
   BASESCAN_API_KEY,
   BSCSCAN_API_BASE_URL,
@@ -9,58 +10,63 @@ import {
   ETHERSCAN_API_KEY,
   OPTIMISMSCAN_API_BASE_URL,
   OPTIMISMSCAN_API_KEY,
-} from '@constants/explorers';
-import type { NextRequest } from 'next/server';
+} from "@constants/explorers";
+import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  const address = searchParams.get('address');
-  const action = searchParams.get('action');
-  const chain = searchParams.get('chain');
-  const page = searchParams.get('page');
-  const offset = searchParams.get('offset');
+  const address = searchParams.get("address");
+  const action = searchParams.get("action");
+  const chain = searchParams.get("chain");
+  const page = searchParams.get("page");
+  const offset = searchParams.get("offset");
 
   // Determine the appropriate API base URL and API key based on the chain
   let apiUrl: string;
   let apiKey: string | null;
 
   switch (chain?.toUpperCase()) {
-    case 'ETH': {
+    case "ETH": {
       apiUrl = ETHERSCAN_API_BASE_URL;
       apiKey = ETHERSCAN_API_KEY;
       break;
     }
-    case 'BASE': {
+    case "BASE": {
       apiUrl = BASESCAN_API_BASE_URL;
       apiKey = BASESCAN_API_KEY;
       break;
     }
-    case 'OP': {
+    case "BASE-SEPOLIA": {
+      apiUrl = BASESCAN_API_BASE_SEPOLIA_URL;
+      apiKey = BASESCAN_API_KEY;
+      break;
+    }
+    case "OP": {
       apiUrl = OPTIMISMSCAN_API_BASE_URL;
       apiKey = OPTIMISMSCAN_API_KEY;
       break;
     }
-    case 'ARB': {
+    case "ARB": {
       apiUrl = ARBSCAN_API_BASE_URL;
       apiKey = ARBSCAN_API_KEY;
       break;
     }
-    case 'BSC': {
+    case "BSC": {
       apiUrl = BSCSCAN_API_BASE_URL;
       apiKey = BSCSCAN_API_KEY;
       break;
     }
     default:
-      return Response.json({ error: 'Invalid chain type' }, { status: 400 });
+      return Response.json({ error: "Invalid chain type" }, { status: 400 });
   }
 
   const query = `module=account&action=${action}&address=${address}&page=${page}&offset=${offset}&startblock=0&endblock=99999999&apikey=${apiKey}`;
 
   const res = await fetch(`${apiUrl}?${query}`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'PostmanRuntime/7.40.0',
+      "Content-Type": "application/json",
+      "User-Agent": "PostmanRuntime/7.40.0",
     },
   });
   const data = await res.json();
--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -1,105 +1,121 @@
-'use client';
-import { BASE_SEPOLIA_CHAIN_ID } from '@/constants';
-import { generatePinataKey, uploadJson } from '@/lib/pinata';
-import { toPng } from 'html-to-image';
-import Image from 'next/image';
-import { useRef, useState } from 'react';
-import type { MintResponse } from '../api/cdp/mint/route';
-export default function Instamint() {
-  const elRef = useRef<HTMLDivElement>(null);
-  const [isMinting, setIsMinting] = useState(false);
-  const [isMintSuccessful, setIsMintSuccessful] = useState(false);
-  const [mintData, setMintData] = useState<MintResponse | null>(null);
+"use client";
+import {} from "@/lib/pinata";
+import Image from "next/image";
+import { useEffect } from "react";
+import { listEVMScanNFTActivity } from "../api/evmScanCallers";
 
-  // TODO: Pass in walletAddress/ENS
-  const toAddress = '0xe7aFE0e83cb9592285c4F92ee396f24c73D93897';
+type Props = {
+  toAddress: string;
+  blockHash: string;
+};
+export default function Instamint({ toAddress, blockHash }: Props) {
+  const getTransactionDetail = async () => {
+    const data = await listEVMScanNFTActivity(
+      "0xC94Fe41573d28e9D9dc4d46593B78c764a4BfE29", // TODO: Replace with toAddress
+      "BASE-SEPOLIA",
+      3 // Get 3 latest transaction
+    );
 
-  const dataURLtoBlob = (dataURL: string) => {
-    const byteString = atob(dataURL.split(',')[1]);
-    const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
-    const ab = new ArrayBuffer(byteString.length);
-    const ia = new Uint8Array(ab);
-    for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-    return new Blob([ab], { type: mimeString });
+    const matchingTransaction = data.find(
+      (transaction) =>
+        transaction.blockHash.toLowerCase() ===
+        "0x80c9831dffc600e7466b8adc62f14513708431564bc6724b628e5a7dc468749f".toLowerCase()
+    );
+
+    console.log("matchingTransaction", matchingTransaction);
+    console.log("tokenID", matchingTransaction?.tokenId);
   };
 
-  const onMint = async () => {
-    setIsMinting(true);
-    try {
-      // Upload image to pinata
-      const keyData = await generatePinataKey();
-      let dataUrl = '';
-      if (elRef.current) {
-        // Capture the component as a PNG image
-        dataUrl = await toPng(elRef.current);
-      } else {
-        throw 'Ref is not available';
-      }
+  useEffect(() => {
+    getTransactionDetail();
+  }, []);
 
-      // TODO: Enable dynamic image on prod
+  // const dataURLtoBlob = (dataURL: string) => {
+  //   const byteString = atob(dataURL.split(',')[1]);
+  //   const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
+  //   const ab = new ArrayBuffer(byteString.length);
+  //   const ia = new Uint8Array(ab);
+  //   for (let i = 0; i < byteString.length; i++) {
+  //     ia[i] = byteString.charCodeAt(i);
+  //   }
+  //   return new Blob([ab], { type: mimeString });
+  // };
 
-      // Convert the data URL to a Blob so it can be uploaded
-      // const blob = dataURLtoBlob(dataUrl);
-      // const fileCID = await uploadFile(blob, keyData.JWT);
-      const fileCID =
-        'bafkreiae3upg3asjkfu5ogi7cah7lyjjxg27jmdzmalfkzfqlvfhqa6rue';
+  // const onMint = async () => {
+  //   setIsMinting(true);
+  //   try {
+  //     // Upload image to pinata
+  //     const keyData = await generatePinataKey();
+  //     let dataUrl = '';
+  //     if (elRef.current) {
+  //       // Capture the component as a PNG image
+  //       dataUrl = await toPng(elRef.current);
+  //     } else {
+  //       throw 'Ref is not available';
+  //     }
 
-      const metadata = {
-        name: `Onchain Buster Trait - ${toAddress}`,
-        description: 'Generated image from Onchain Buster',
-        image: fileCID,
-        external_url: 'https://onchainbuster.vercel.app',
-      };
+  //     // TODO: Enable dynamic image on prod
 
-      const uriCID = await uploadJson(metadata, keyData.JWT);
+  //     // Convert the data URL to a Blob so it can be uploaded
+  //     // const blob = dataURLtoBlob(dataUrl);
+  //     // const fileCID = await uploadFile(blob, keyData.JWT);
+  //     const fileCID =
+  //       'bafkreiae3upg3asjkfu5ogi7cah7lyjjxg27jmdzmalfkzfqlvfhqa6rue';
 
-      console.log('Initiating minting process');
-      const response = await fetch('/api/cdp/mint', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          networkId: BASE_SEPOLIA_CHAIN_ID,
-          to: toAddress,
-          uri: uriCID,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data: MintResponse = await response.json();
-      console.log('Mint response:', data);
-      // Store the response data
-      setMintData(data);
-      setIsMintSuccessful(true);
-    } catch (error) {
-      console.error('Error during minting:', error);
-      alert('Failed to mint. Please try again.');
-    } finally {
-      setIsMinting(false);
-    }
-  };
-  const onViewMintTransaction = () => {
-    // Implement view transaction logic
-    console.log('View transaction');
-    if (mintData?.mintTxUrl) {
-      window.open(mintData.mintTxUrl, '_blank');
-    } else {
-      console.error('Mint transaction URL is not available');
-    }
-  };
+  //     const metadata = {
+  //       name: `Onchain Buster Trait - ${toAddress}`,
+  //       description: 'Generated image from Onchain Buster',
+  //       image: fileCID,
+  //       external_url: 'https://onchainbuster.vercel.app',
+  //     };
+
+  //     const uriCID = await uploadJson(metadata, keyData.JWT);
+
+  //     console.log('Initiating minting process');
+  //     const response = await fetch('/api/cdp/mint', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         networkId: BASE_SEPOLIA_CHAIN_ID,
+  //         to: toAddress,
+  //         uri: uriCID,
+  //       }),
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
+  //     const data: MintResponse = await response.json();
+  //     console.log('Mint response:', data);
+  //     // Store the response data
+  //     setMintData(data);
+  //     setIsMintSuccessful(true);
+  //   } catch (error) {
+  //     console.error('Error during minting:', error);
+  //     alert('Failed to mint. Please try again.');
+  //   } finally {
+  //     setIsMinting(false);
+  //   }
+  // };
+  // const onViewMintTransaction = () => {
+  //   // Implement view transaction logic
+  //   console.log('View transaction');
+  //   if (mintData?.mintTxUrl) {
+  //     window.open(mintData.mintTxUrl, '_blank');
+  //   } else {
+  //     console.error('Mint transaction URL is not available');
+  //   }
+  // };
 
   return (
     <>
       <div className="flex flex-col items-center min-h-screen p-4 w-full">
         <h1 className="text-4xl font-bold text-blue-600 mb-8">
           Test Mint Dialog
-          <div ref={elRef}>
+          <div>
             <Image
-              src={'/logo.png'}
+              src={"/logo.png"}
               alt="Modal image"
               width={300}
               height={300}
@@ -109,17 +125,17 @@ export default function Instamint() {
           </div>
         </h1>
 
-        <button
+        {/* <button
           type="button"
           onClick={onMint}
-          className={'bg-blue-500 hover:bg-blue-600'}
+          className={"bg-blue-500 hover:bg-blue-600"}
         >
           Mint it!
         </button>
 
         <button type="button" onClick={onViewMintTransaction}>
           View mint transaction
-        </button>
+        </button> */}
       </div>
     </>
   );
