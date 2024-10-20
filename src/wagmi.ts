7d53b3f45c0bd186bc54733fad866245ab508414
Minh Pham <pcminh0505@gmail.com>
2024-10-20 14:06:01 +0700

    chore: Add Coin98 Super Wallet

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,20 +1,21 @@
-'use client';
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+"use client";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
 import {
+  coin98Wallet,
   coinbaseWallet,
   metaMaskWallet,
   rainbowWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-import { useMemo } from 'react';
-import { http, createConfig } from 'wagmi';
-import { base, baseSepolia, mainnet, sepolia } from 'wagmi/chains';
-import { NEXT_PUBLIC_WC_PROJECT_ID } from './config';
+} from "@rainbow-me/rainbowkit/wallets";
+import { useMemo } from "react";
+import { http, createConfig } from "wagmi";
+import { base, baseSepolia, mainnet, sepolia } from "wagmi/chains";
+import { NEXT_PUBLIC_WC_PROJECT_ID } from "./config";
 
 export function useWagmiConfig() {
-  const projectId = NEXT_PUBLIC_WC_PROJECT_ID ?? '';
+  const projectId = NEXT_PUBLIC_WC_PROJECT_ID ?? "";
   if (!projectId) {
     const providerErrMessage =
-      'To connect to all Wallets you need to provide a NEXT_PUBLIC_WC_PROJECT_ID env variable';
+      "To connect to all Wallets you need to provide a NEXT_PUBLIC_WC_PROJECT_ID env variable";
     throw new Error(providerErrMessage);
   }
 
@@ -22,18 +23,18 @@ export function useWagmiConfig() {
     const connectors = connectorsForWallets(
       [
         {
-          groupName: 'Recommended Wallet',
+          groupName: "Recommended Wallet",
           wallets: [coinbaseWallet],
         },
         {
-          groupName: 'Other Wallets',
-          wallets: [rainbowWallet, metaMaskWallet],
+          groupName: "Other Wallets",
+          wallets: [rainbowWallet, metaMaskWallet, coin98Wallet],
         },
       ],
       {
-        appName: 'onchainkit',
+        appName: "onchainkit",
         projectId,
-      },
+      }
     );
 
     const wagmiConfig = createConfig({
